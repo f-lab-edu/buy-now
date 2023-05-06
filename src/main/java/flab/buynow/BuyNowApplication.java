59d82f04d43916d12c03a600174cbff9312d8166
j-jeongeun <jejeong88@gmail.com>
2023-05-07 01:08:47 +0900

    project settings and initial commit

---
+++ src/main/java/flab/buynow/BuyNowApplication.java
@@ -0,0 +1,13 @@
+package flab.buynow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/buynow/BuyNowApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.buynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
